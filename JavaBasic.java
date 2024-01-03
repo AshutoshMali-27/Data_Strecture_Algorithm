db039785adc6aae909de0483f7af3e11b79db37c
ashutosh <ashutoshmali67@gmail.com>
2024-01-03 23:42:48 +0530

    first commit

---
+++ JavaBasic.java
@@ -0,0 +1,10 @@
+public class JavaBasic{
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+          System.out.print("Hello World \n");
+            System.out.print("Hello World \n");
+    }
+}
+//boilerplate code
