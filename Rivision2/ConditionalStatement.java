8216efbda476140c94af9b963020f2666eb44b57
ashutosh <ashutoshmali67@gmail.com>
2024-05-03 23:54:57 +0530

    commit all

---
+++ Rivision2/ConditionalStatement.java
@@ -0,0 +1,24 @@
+package Rivision2;
+
+import java.util.Scanner;
+
+public class ConditionalStatement {
+    
+public static void main(String[] args){
+
+Scanner sc=new Scanner(System.in);
+int age=sc.nextInt();
+
+
+if(age>=18){
+    System.out.println("person is Adult");
+}else{
+    System.out.println("person is non adult");
+}
+
+
+}
+
+
+
+}
---
+++ Rivision2/tutorial.java
@@ -0,0 +1,34 @@
+package Rivision2;
+
+import java.util.Scanner;
+
+public class tutorial {
+
+    public static void main(String[] args){
+
+    //     Scanner sc= new Scanner(System.in);
+    //    int a= sc.nextInt();
+    //    int b= sc.nextInt();
+    //    int c= sc.nextInt();
+       
+    
+
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+
+
+    String  name="ashutosh";
+
+    int sum;
+    sum=a+b+c;
+    System.out.println(name);
+
+
+
+
+    }
+
+    
+}
